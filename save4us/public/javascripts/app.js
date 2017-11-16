da7c79b3b2169cf24c401c583328ad6c279b2779
Chris Cutler <chris.b.cutler@gmail.com>
2017-11-16 16:59:06 -0700

    code

--- save4us/public/javascripts/app.js
+++ save4us/public/javascripts/app.js
@@ -1,10 +1,14 @@
 angular.module('Save', [])
 .controller('mainCtrl', ['$scope', '$http', function($scope, $http){
 	$scope.goals = [];
-	$scope.goals.push({title: "car", total: "1000", balance:"5"});
+	$scope.goals.push({title: "car", total: "1000", balance:"5", percentage:75});
 
 	$scope.addGoal = function() {
-		var newGoal = {title:$scope.title, total:$scope.total, balance:$scope.balance};
+		var total = parseInt($scope.total);
+		var balance = parseInt($scope.balance);
+		var per = (balance / total) * 100;
+
+		var newGoal = {title:$scope.title, total: $scope.total, balance: $scope.balance, percentage: per};
 		$scope.title = "";
 		$scope.total = "";
 		$scope.balance = "";
