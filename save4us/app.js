d9f2b8143c16d6d92746dcd6872a702b77ea9359
Tyler Grant <tyler.grant.2010@gmail.com>
2017-11-16 01:10:45 +0000

    Forgot a line

--- save4us/app.js
+++ save4us/app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/saveDB',{useMongoClient:true});
+require('./models/save');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
