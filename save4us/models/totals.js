21e8beb646fcfc2b866ce49a601909922a1fd576
Tyler Grant <tyler.grant.2010@gmail.com>
2017-11-17 00:45:05 +0000

    Update balance tweaked

--- save4us/models/totals.js
+++ save4us/models/totals.js
@@ -3,11 +3,11 @@ var TotalSchema = new mongoose.Schema({
   title: String,
   total: {type: Number, default: 0},
   balance: {type: Number, default: 0},
-  percentage: {type: Number, default: 0}
+  percentage: {type: Number, default: 0},
   add: {type: Number, default: 0}
 });
 
-TotalScheme.methods.updateBalance = function(cb) {
+TotalSchema.methods.updateBalance = function(cb) {
 	this.balance += add;
 	this.save(cb);
 }
--- save4us/public/javascripts/app.js
+++ save4us/public/javascripts/app.js
@@ -1,24 +1,30 @@
 angular.module('Save', [])
 .controller('mainCtrl', ['$scope', '$http', function($scope, $http){
 	$scope.goals = [];
-	$scope.goals.push({title: "car", total: "1000", balance:"5", percentage:75});
+	$scope.goals.push({title: "car", total: "1000", balance:"5"});
 
 	$scope.addGoal = function() {
-		var total = parseInt($scope.total);
-		var balance = parseInt($scope.balance);
-		var per = (balance / total) * 100;
-
-		var newGoal = {title:$scope.title, total: $scope.total, balance: $scope.balance, percentage: per};
+		var newGoal = {title:$scope.title, total:$scope.total, balance:$scope.balance, add: 0};
 		$scope.title = "";
 		$scope.total = "";
 		$scope.balance = "";
+
 		$http.post("/addGoal", newGoal).success(function(data){
 			$scope.goals.push(data);
 		});
 	};
 
-	$scope.updateBalance = function(goal) {
+	$scope.addFunds = function(total) {
+		return $http.put('/totals/' + total._id + '/updateBalance')
+		.success(function(data){
+			console.log("addFunds worked");
+			total.balance = data.balance;
+		});
+	};
 
+	$scope.updateBalance = function(goal) {
+		goal.add = $scope.additionalFunds;
+		$scope.addFunds(goal);
 	}
 
 	$scope.getAll = function() {
